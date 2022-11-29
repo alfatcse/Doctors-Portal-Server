458ad238e10b12168ea2baa51e6071e8a88e20ac
Alfat <alfat.cse.21@gmail.com>
2022-11-29 10:05:42 +0100

    mongodb client set up

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const {MongoClient} = require('mongodb');
 const port=process.env.PORT||5006;
 const app=express();
 app.use(cors());

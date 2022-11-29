496ab0d3035bb6ef10dc703a7e379a2103d8a4e1
Alfat <alfat.cse.21@gmail.com>
2022-11-29 10:02:19 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const port=process.env.PORT||5006;
+const app=express();
+app.use(cors());
+app.use(express.json());
+app.get('/',async(req,res)=>{
+    res.send('Doctors Portal Server Running');
+})
+app.listen(port,()=>console.log(`Doctors Portal Server Running ${port}`))

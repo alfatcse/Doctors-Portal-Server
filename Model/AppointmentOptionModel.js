7150e242ccf432b5a1d0b2dc4d3401c6a1665c64
Alfat <alfat.cse.21@gmail.com>
2023-05-09 21:39:48 +0200

    appointment Option model implemented

---
+++ Model/AppointmentOptionModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const appointmentOptions = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please Provide a Service Name"],
+    lowercase: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  slots: Array,
+  doctors: [
+    {
+      name: {
+        type: String,
+        required: [true, "Please Provide a Service Name"],
+        lowercase: true,
+        trim: true,
+      },
+      docEmail: {
+        type: String,
+        required: true,
+        unique: true,
+        max: 50,
+        lowercase: true,
+        validate: [validator.isEmail, "Please provide a valid email"],
+      },
+    },
+  ],
+},{ versionKey: false });
+module.exports=mongoose.model("appointmentOptions",appointmentOptions,"appointmentOptions");

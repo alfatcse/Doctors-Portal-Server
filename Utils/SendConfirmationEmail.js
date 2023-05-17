3ef16d6917e955023a72bdb9aa74be1dfe28b70f
Alfat <alfat.cse.21@gmail.com>
2023-05-17 14:12:16 +0200

    confirmation mail send by nodemailer

--- Utils/SendConfirmationEmail.js
+++ Utils/SendConfirmationEmail.js
@@ -1,37 +1,40 @@
-const mg = require("mailgun-js");
-exports.sendBookingEmail = async (booking) => {
+const nodemailer = require("nodemailer");
+const sendBookingEmail = (booking) => {
+  console.log(booking);
+  let config = {
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PASS,
+    },
+  };
   const {
     patient_email: email,
     slot,
     treatment,
     AppointmentDate: appointmentDate,
   } = booking;
-  const transporter = () =>
-    mg({
-      apiKey: process.env.EMAIL_SEND_KEY,
-      domain: process.env.EMAIL_SEND_DOMAIN,
-    });
-  const emailInfo = {
-    from: "alfatjahanbsmrstu@gmail.com", // verified sender email
-    to: email, // recipient email
-    subject: `Your Appointment for ${treatment} is confirmed`, // Subject line
-    text: "Doctor's Portal Booking Confirmation!", // plain text body
-    html: `
-               <h3>Your appointment is confirmed</h3>
-               <div> 
+  let transporter = nodemailer.createTransport(config);
+  let message = {
+    from: process.env.EMAIL_ADDRESS,
+    to: booking.patient_email,
+    subject: "Appointment Confirmation",
+    html: `<h3>Your appointment is confirmed</h3>
+            <div> 
                 <p> Your appointment for treatment ${treatment}</p>
                 <p> Please Visit us on ${appointmentDate} at ${slot}</p>
-                <p>Thanks from Doctor'‚s Portal</>
-               </div>
-          `, // html body
+                <p>Thanks from Doctor's Portal</>
+            </div>`, 
   };
-  await transporter()
-    .messages()
-    .send(emailInfo, (error, body) => {
-      if (error) {
-        console.log("An Error Occurred",error);
-      } else {
-        console.log("Email Send Successfully");
-      }
+  transporter
+    .sendMail(message)
+    .then(() => {
+      console.log("done");
+    })
+    .catch((error) => {
+      console.log(error);
     });
+};
+module.exports = {
+  sendBookingEmail,
 };
